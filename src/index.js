ef66937147cbcb223181dd5b5edbfed2927a9f63
duongpq_NALS <duongpq@nal.vn>
2021-12-17 21:37:38 +0700

    up code day9

--- src/index.js
+++ src/index.js
@@ -4,12 +4,33 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function time (){
+  const date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date. getUTCDate();
+  
+  let hour = date.getUTCHours() +7;
+  let minute = date.getMinutes();
+  let second = date.getUTCSeconds();
+  
+  let elemet = (
+    
+  <h1> {hour}:{minute}:{second} - {day}/ {month}/{year}</h1>
+    
+  )
+  ReactDOM.render(elemet, document.getElementById('root'))
+}
+setInterval(time,1000)
+// ReactDOM.render(
+//   <div>
+//     <h1>Hello, world!</h1>
+//     <h2>It is {new Date().toLocaleTimeString()}.</h2>
+//   </div>,
+//   document.getElementById('root')
+// );
+// // setInterval(tick, 1000);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
